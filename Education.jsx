6b9fdf89384430bfc0ce3f9f283aff92d5f9c9c2
Julia <juliafrare7@gmail.com>
2022-06-06 13:48:03 -0300

    index page with resume tabs

---
+++ Education.jsx
@@ -0,0 +1,11 @@
+import './jobExperience.css'
+
+const Education = () => {
+    return (
+        <div className="content">
+            <p>Bachelor's of Computer Science @ University of São Paulo</p>
+        </div>
+    );
+}
+
+export { Education };
