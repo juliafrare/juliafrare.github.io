5fdc06915da8af05e3465c57f9f91a4f8e5737c0
Julia <juliafrare7@gmail.com>
2022-07-02 22:03:00 -0300

    refactor to typescript

---
+++ src/Tabs.tsx
@@ -0,0 +1,49 @@
+const Education = () => {
+    return (
+        <div>
+            <p>Bachelor's of Computer Science @ University of São Paulo</p>
+        </div>
+    );
+}
+
+const JobExperience = () => {
+    return (
+        <div>
+            <p>Software Engineering Intern @ Amdocs: 11/2021 - present</p>
+            <p>Software Development Intern @ Já Vendeu: 01/2021 - 10/2021</p>
+        </div>
+    );
+}
+
+
+const Activities = () => {
+    return (
+        <div>
+            <p>2D Artist @ Fellowship of the Game: August 2021 - present</p>
+            <p>Event Organizer and Back End Developer @ Semcomp: April 2021 - October 2021</p>
+            <p>Volunteer and Coordinator @ PET Computação: August 2019 - August 2021</p>
+            <p>Member @ Ganesh ICMC: August 2018 - August 2019</p>
+        </div>
+    );
+}
+
+const Projects = () => {
+    return (
+        <div>
+            <p>This website :)</p>
+        </div>
+    );
+}
+
+const Skills = () => {
+    return (
+        <div>
+            <p><b>Programming Languages:</b> Python, C, C++, C#, Java, HTML, CSS, Javascript, SQL</p>
+            <p><b>Tools & Frameworks:</b> Node.js, React, React Native, Git, Unity, Docker</p>
+            <p><b>Databases:</b> MySQL, PostgreSQL, MongoDB</p>
+            <p><b>Languages:</b> Portuguese (Native), English (Advanced), Spanish (Intermediate)</p>
+        </div>
+    );
+}
+
+export { Education, JobExperience, Activities, Projects, Skills };
---
+++ src/TabsComponent.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import './TabsComponent.css'
+import { Activities, Education, JobExperience, Projects, Skills } from "./Tabs";
+
+const tabItems = [
+    {
+        id: 1,
+        title: 'Education',
+        content: <Education />,
+    },
+    {
+        id: 2,
+        title: 'Job Experience',
+        content: <JobExperience />,
+    },
+    {
+        id: 3,
+        title: 'Activities',
+        content: <Activities />,
+    },
+    {
+        id: 4,
+        title: 'Projects',
+        content: <Projects />,
+    },
+    {
+        id: 5,
+        title: 'Skills',
+        content: <Skills />,
+    },
+];
+
+const TabsComponent: React.FC = () => {
+    const [active, setActive] = useState(0);
+     
+    return (
+        <div className="wrapper">
+            <div className="tabs">
+                {
+                    
+                }
+                {tabItems.map(({ id, title }) =><Tab
+                    key={title}
+                    title={title}
+                    onItemClicked={() => setActive(id)}
+                    isActive={active === id}
+                    />
+                )}
+            </div>
+            <div className="content">
+                {tabItems.map(({ id, content }) => {
+                    return active === id ? content : ''
+                })}
+            </div>
+        </div>
+    );
+}
+
+const Tab = ({
+    icon = '',
+    title = '',
+    onItemClicked = () => console.error('You passed no action to the component'), isActive = false,}) => {
+        return (
+            <div className={isActive ? 'tabitem' : 'tabitem tabitem--inactive'} onClick={onItemClicked}>
+                <i className={icon}></i>
+                <p className="tabitem__title">{title}</p>
+            </div>
+        )
+}
+
+export default TabsComponent;
