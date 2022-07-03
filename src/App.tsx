3b43faf1bc1cf089b02ddd8b74fd65e662614425
Julia <juliafrare7@gmail.com>
2022-07-03 01:31:00 -0300

    removes unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { TextField } from './TextField';
 import TabsComponent from './TabsComponent';
 import { Header } from './Header';
 import { Socials } from './Socials';
